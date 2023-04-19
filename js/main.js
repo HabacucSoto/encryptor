b78cf04689a255fbab56b67ea1256d72f64c1d86
Habacuc Soto <josehabacuc182020@gmail.com>
2023-04-19 07:44:07 -0600

    Add dom components to work in code

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// ! DOM components
+const input = document.getElementById("input").value;
+const img = document.getElementById("img");
+const encrypt = document.getElementById("encrypt");
+const decrypt = document.getElementById("decrypt");
+const copy = document.getElementById("copy");
+const clear = document.getElementById("clear");
+
+
